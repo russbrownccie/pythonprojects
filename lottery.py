16bedfb6b6f6449f35629fa505889fb555004c50
Russell Brown <78993850+russbrownccie@users.noreply.github.com>
2022-06-03 13:32:19 -0800

    Create lottery.py
    just a fun thing making the PC draw for powerballs till I get a full match

---
+++ lottery.py
@@ -0,0 +1,39 @@
+# Powerball Lottery Simulator - v 0.1
+# Written by Russ Brown for Python Crash Course project
+# This program attempts to continuously draw for powerball until it gets a match on the MY_TICKET variable
+# Powerball draws 5 numbers from a pool of white balls 1-69 and one red ball 1-26
+# This program saves those as a dictionary, and then loops.   When the entire drawing is done, it compares tickets
+# The program will tell you how long it took and how many tries - 400 million tries could take up to an hour on mine
+# Written totally for fun - gambling is bad, kids
+
+from random import randint
+import time
+
+# Make sure your green entries are sorted low to high or you'll never get a match
+MY_TICKET = {"green": [27, 28, 51, 68, 69], "red": 22}
+
+drawing = 0
+start_time = time.time()
+match = False
+while not match:
+    power_balls = {}
+    white_drawing = []
+    drawing += 1
+    while len(white_drawing) <= 4:
+        candidate = randint(1, 69)
+        if candidate not in white_drawing:
+            white_drawing.append(candidate)
+    white_balls = sorted(white_drawing)
+    power_balls["green"] = white_balls
+    power_balls["red"] = randint(1, 26)
+
+    if power_balls == MY_TICKET:
+        print(f'We have a match! {power_balls} and it took {drawing} tries to get a win')
+        end_time = time.time()
+        total_time = round(end_time - start_time, 2)
+        print(f"It took {total_time} seconds to generate {drawing} tries")
+        match = True
+
+# Sample output 
+# We have a match! {'green': [27, 28, 51, 68, 69], 'red': 22} and it took 231954286 tries to get a win
+# It took 1015.89 seconds to generate 231954286 tries
