3e3b52565aa5119ae841c3d65977f26bc52660ff
Russell Brown <78993850+russbrownccie@users.noreply.github.com>
2022-06-03 19:28:45 -0800

    Update workout.py

--- workouttracker/workout.py
+++ workouttracker/workout.py
@@ -1,23 +1,24 @@
-# Workout (Expressway) tracker - Russ Brown - v0.1
+# Workout (Expressway) tracker - Russ Brown - v0.2
 # This is a start at making a tracker for my Expressway workouts - (one set, 12 reps, 2 sec extend, 4 sec contract)
-# this pulls the workouts and my weights from a file called workouts.txt
+# this pulls the workouts and my last weights from a file called workouts.json
 # The file is in dictionary format - more exercise can be added as one wishes
-# The idea is you do 12 reps in one set 
+# The idea is you do 12 reps at 5 pounds higher than your last workout
 # If you succeed in doing 12 reps, all the weights are bumped up by 5 lbs (should be 5% but the machines are in lbs)
-# If you struggled, it will ask which exercises (must type the exact exercise but it will convert to title for you)
+# If you struggled, it will ask which exercises (should type the exact exercise but it will convert to title for you)
 # It will then ask you want to do the same weight again next time, or take it down a step
 # Future modifications might include taking the input as soon as the exercise is done (assuming done in order)
-# Some input validation - maybe track each exercise as you go and then you won't forget
-# Afterwards, it will write back to workouts.txt with the new values
+# Afterwards, it will right back to workouts.json with the new values
 # I run this in replit so I just browse to the replit and start running it while I do my workout
-# Feel free to use for your own benefit
 
-import ast  # needed to convert the str to dict when pulled from workouts.txt
-# open workout and convert to a dictionary
-with open("workouts.txt") as file:
-    data = file.read()
 
-workout = ast.literal_eval(data)
+# changes in v0.2 - changed file load process to json for easier processing
+
+import json
+filename = 'workouts.json'
+with open(filename) as file:
+    workout = json.load(file)
+print(type(workout))
+
 # print out the current workout for this session
 print('For This Workout:\n\n')
 for k, v in workout.items():
@@ -42,6 +43,6 @@ else:
         else:
             missed_exercises = False
 
-# finally - write the new dictionary to the workouts.txt and overwrite the old value with new dict
-with open('workouts.txt', 'w') as file:
-    file.write(str(next_workout))
+# finally - write the new dictionary to the workouts.json and overwrite the old value with new dict
+with open('workouts.json', 'w') as file:
+    json.dump(next_workout, file)
